d873963378f1ac382391746f367688df6e10995d
Yusuf-Firmansyah <yusuffirmansyamh@gmail.com>
2021-08-18 09:05:19 +0700

    initialize repo and first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+
+const Mode = () => {
+  const handleClick = () => {
+    // console.log("click");
+
+    const nameMode = document.getElementsByClassName("nameMode")[0];
+    const mode = document.getElementsByTagName("div")[1];
+    const mode2 = mode.classList.toggle("section");
+
+    // console.log(nameMode);
+    if (mode2) {
+      return (nameMode.innerHTML = "Light");
+    } else {
+      return (nameMode.innerHTML = "Dark");
+    }
+  };
+
+  return (
+    <div className="mode" onClick={handleClick}>
+      <p className="nameMode">Dark</p>
+    </div>
+  );
+};
+
+const Header = ({ title }) => {
+  return <h1 className="header"> {title} </h1>;
+};
+
+const TasksApp = () => {
+  const [value, setValue] = useState("");
+  const [items, setItems] = useState([]);
+
+  const handleForm = (e) => {
+    e.preventDefault();
+
+    setItems([...items, value]);
+    setValue("");
+  };
+
+  return (
+    <>
+      {" "}
+      {items <= 1 ? (
+        ""
+      ) : (
+        <p className="notif">
+          {" "}
+          Ada <strong style={{ color: "red" }}>{items.length}</strong> tasks
+          yang belum anda selesaikan
+        </p>
+      )}
+      <form onSubmit={handleForm}>
+        <input
+          type="text"
+          value={value}
+          placeholder="selesaikan"
+          required
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+        />
+        <button> Tambah </button>
+      </form>
+      {items.map((item, index) => {
+        return (
+          <>
+            <div className="section-tasks">
+              <span key={index}> {item} </span>
+
+              <button
+                style={{ cursor: "pointer" }}
+                onClick={() =>
+                  setItems(
+                    items.filter((_, filterindex) => filterindex !== index),
+                  )
+                }
+              >
+                {" "}
+                Clear
+              </button>
+            </div>
+          </>
+        );
+      })}
+    </>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="App">
+        <Header title="Apa yang ingin anda lakukan ?" />
+        <TasksApp />
+        <Mode />
+      </div>
     </div>
   );
 }
